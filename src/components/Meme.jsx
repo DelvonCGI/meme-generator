21bb43bb918c6ec45212041c7cdf99e8e073c6cc
Dasigma <delvon.fontenot@cgi.com>
2022-09-08 22:55:36 -0500

    continuation

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,10 +1,26 @@
-import memesData from "../memsData"
+import memesData from "../memesData"
+import { useState } from "react";
 
 export default function Meme(){
-   function handleOnClick() {
+
+   //const [memeImage, setMemeImage] = useState('http://i.imgflip.com/1bij.jpg');
+   const [meme, setMeme] = useState({
+      topText: "Top",
+      bottomText: "Bottom",
+      randomImage: "http://i.imgflip.com/1bij.jpg"
+   })
+
+
+
+   const [allMemeImages, setAllMemeImages] = useState(memesData)
+
+   function getmemeImage() {
       let list = memesData.data.memes
-      let item = list[Math.floor((Math.random()*list.length))]
-      console.log(item.url)
+      let memeImage = list[Math.floor((Math.random()*list.length))]
+      setMeme({
+         ...meme,
+         randomImage: memeImage.url
+      })
    }
 
    return (
@@ -12,8 +28,11 @@ export default function Meme(){
          <div className="form">
          <input className="form--input" type='text' placeholder="Top text"></input>
          <input className="form--input" type='text' placeholder="Bottom text"></input>
-         <button onClick={handleOnClick} className="form--button" value='Get a new meme image'>Get a new meme image 🖼</button>
+         <button onClick={getmemeImage} className="form--button" value='Get a new meme image'>Get a new meme image 🖼</button>
          </div>
+         <img src={meme.randomImage} className="meme--image" />
+         <span className="toptext--text">{meme.topText}</span>
+         <span className="bottomtext--text">{meme.bottomText}</span>
       </main>
 
    )
